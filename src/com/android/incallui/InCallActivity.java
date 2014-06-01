872aef09974653fcada126cd7c6f011089ace2b0
Jorge Ruesga <jorge@ruesga.com>
2014-06-02 01:37:42 +0200

    incallui: make navigation bar translucent + other fixes
    * Make navigation bar translucent when available, minimizing thec bad
      effect of the black navigation bar.
    * Complete change Ie76cbc53c176d94bfb2402bf50eae6161fbb2198, displaying
      the caller photo in complete full window size.
    * Sync Msim layouts with normal layouts
    
    Patchset 3: Use full translucent bars (status + navigation)
    Patchset 4: Revert layouts
    Patchset 5: Simplification, use transparent status bar during call
    Patchset 6: Fix dialpad layout
    Patchset 7: update translucency flags when hiding conference manager
    Patchset 8: also make status bar translucent when dialpad is shown
    
    Change-Id: I3a9e4e248f19b951dd6a127b6f63a0cd62047497
    JIRA: CYAN-4048
    Issue: https://jira.cyanogenmod.org/browse/CYAN-4048
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/incallui/InCallActivity.java
+++ src/com/android/incallui/InCallActivity.java
@@ -66,6 +66,7 @@ public class InCallActivity extends Activity {
 
     /** Use to pass 'showDialpad' from {@link #onNewIntent} to {@link #onResume} */
     private boolean mShowDialpadRequested;
+    private boolean mConferenceManagerShown;
 
     @Override
     protected void onCreate(Bundle icicle) {
@@ -124,6 +125,7 @@ public class InCallActivity extends Activity {
             mCallButtonFragment.displayDialpad(true);
             mShowDialpadRequested = false;
         }
+        updateSystemBarTranslucency();
     }
 
     // onPause is guaranteed to be called when the InCallActivity goes
@@ -239,6 +241,8 @@ public class InCallActivity extends Activity {
             return;
         } else if (mConferenceManagerFragment.isVisible()) {
             mConferenceManagerFragment.setVisible(false);
+            mConferenceManagerShown = false;
+            updateSystemBarTranslucency();
             return;
         }
 
@@ -469,7 +473,28 @@ public class InCallActivity extends Activity {
     public void displayManageConferencePanel(boolean showPanel) {
         if (showPanel) {
             mConferenceManagerFragment.setVisible(true);
+            mConferenceManagerShown = true;
+            updateSystemBarTranslucency();
         }
+    }
+
+    public void onManageConferenceDoneClicked() {
+        if (mConferenceManagerShown && !mConferenceManagerFragment.isVisible()) {
+            mConferenceManagerShown = false;
+            updateSystemBarTranslucency();
+        }
+    }
+
+    private void updateSystemBarTranslucency() {
+        final boolean doTranslucency = !mConferenceManagerShown;
+        final Window window = getWindow();
+
+        if (doTranslucency) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        window.getDecorView().requestFitSystemWindows();
     }
 
     // The function is called when Modify Call button gets pressed.
